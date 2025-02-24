ae8dfb62c9483a944fa12485f4ac2553996e59b2
Jan Leirvaag <lja022025@gmail.com>
2025-02-24 10:24:01 +0100

    test

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Hello World")
+// comment
+var x=5 //var is deprecated, do not use
+let y=25
+const name="Jan"
+let firstName="Jan"
+let exampleFloat=30.2
+let fromBergen = false
+let exampleArray = ["Opel","Ford"]
+
+let myObject = {
+    firstName:"Jan",
+    lastName:"Bones"
+}
+
+console.log(exampleArray)
