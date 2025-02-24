f7f97baeb0d966f00b718bf6d1d8b42285878ce1
Jan Leirvaag <lja022025@gmail.com>
2025-02-24 13:31:13 +0100

    fixed

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 //conditionals oppgave
  
 const userName = "tom";
-const userAge = 19;
- 
+
+let userNameLength = userName.length;
+const userAge = 18;
+
+ if (userNameLength >= 1) {
+    console.log("Username is valid");
+    userNameLength = true;
+ } else {
+    console.log("Username is not valid");
+    userNameLength = false;
+}
+
 let userIsAdmin = true;
 let userIsLoggedIn = false;
 let userIsBlocked = false;
  
 let goToPage = "";
 
-if (userName !== null  || userAge >= 18 || userIsBlocked == false) {
-   // console.log("error:",userName, userAge, userIsBlocked)
+if (userAge >= 18 && userNameLength === true && userName === "tom") 
+   {
+   
+     goToPage = "/home";
+     userIsLoggedIn = true;
+     console.log(goToPage);
+     console.log(userName, userAge, userIsBlocked, userIsLoggedIn,);
     }
 else
 {
-    goToPage = "/home";
-    userIsLoggedIn = true;
-    console.log(goToPage);
-    console.log(userName, userAge, userIsBlocked);
+   
+    console.log("error: ",userName, userAge, userIsBlocked, userIsLoggedIn);
+    console.log(goToPage)
 }
 
