e6f35006f40547c9a8d66adcb7f947592ee24841
Jan Leirvaag <lja022025@gmail.com>
2025-02-25 19:26:35 +0100

    added image swapping

--- Button/index.js
+++ Button/index.js
@@ -1,13 +1,39 @@
 let count = 0
 let CountEl = document.getElementById("count-el")
 
+let img = document.createElement("img");
+
+let picURL = "gfx/sal.png";
+const picSAL = "gfx/sal.png";
+const picSalute = "gfx/Salute.jpg";
+
 function increment() {
     count = count + 1
 
     console.log(count)
     CountEl.innerText = count
-    //document.getElementById("count-el").innerText = count
+
+        
+    showImage(img);
+
 }
+
+function showImage(img) {
+
+    //switches between the two images when button is clicked
+    if (picURL == picSAL) {
+        picURL = picSalute;
+    } else {
+        picURL = picSAL;
+    }
+
+    img.src = picURL;
+
+    document.body.appendChild(img);
+    console.log(picURL);
+    document.replaceChild
+}
+
 
 let Lap1 = 34
 let Lap2 = 35
