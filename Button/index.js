f7f15aad00cb38ac85433db3573645c35dddb2ee
Jan Leirvaag <lja022025@gmail.com>
2025-02-27 13:40:58 +0100

    changed font, added border

--- Button/index.js
+++ Button/index.js
@@ -1,16 +1,10 @@
-let count = 0
-let CountEl = document.getElementById("count-el")
+let count = 0;
+
+let CountEl = document.getElementById("count-el");
 let SaveEl = document.getElementById("save-el");
 
 //test examples
-let WelcomeEl = document.getElementById("welcome-el")
-let username = "Jan";
-let message = "You have three new notifications, ";
-let messageToUser = message + ", "+username + "!";
-console.log(messageToUser);
 
-WelcomeEl.innerText = message + username;
-WelcomeEl.innerText += " :-)"
 //test examples
 
 //image swap test
@@ -21,17 +15,23 @@ let picURL = picSAL;
 //image swap test
 
 function increment() {
+    
     count += 1;
 
-    console.log(count)
-    CountEl.innerText = count
+    console.log(count);
+    CountEl.innerText = count;
 
         
    //showImage();
 }
 
 function save(){
-    console.log(count);
+    let prevCount = count + " - ";
+    SaveEl.textContent += prevCount;
+    count = 0;
+    CountEl.innerText = count;
+    console.log("count: ",count);
+
 }
 
 function showImage() {
